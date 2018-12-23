dd7ed740b05260424693dc463d3ac9e2b5a7a8a5
Nor <nor8111@gmail.com>
2018-12-23 23:35:31 +0200

    language autodetect

--- js/index.js
+++ js/index.js
@@ -10,8 +10,7 @@ function detectLanguage() {
     if (userLang != null && userLang.length >= 2) userLang = userLang.substring(0, 2);
     if (userLang === "uk") setLanguage("ukr");
     else if (userLang === "ru") setLanguage("rus");
-    else setLanguage("en");
-    alert(userLang);
+    else setLanguage("en"); alert(userLang);
 
     return false;
 }
